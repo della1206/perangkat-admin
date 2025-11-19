e424fa7105548d613ca98e21b3642e354569b870
della <della24si@mahasiswa.pcr.ac.id>
2025-11-19 09:24:57 +0700

    ubah sikit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,9 +10,9 @@ class DashboardController extends Controller
     public function index()
     {
         // Jika belum login, kembalikan ke login
-        if (!session()->has('user_id')) {
-            return redirect()->route('login')->with('error', 'Silakan login terlebih dahulu.');
-        }
+        // if (!session()->has('user_id')) {
+        //     return redirect()->route('login')->with('error', 'Silakan login terlebih dahulu.');
+        // }
 
         // Ambil data user
         $user = User::find(session('user_id'));
