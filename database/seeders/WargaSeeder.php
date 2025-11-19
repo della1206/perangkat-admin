1032be94631521943bfb26cc109b3c831dcec88e
della <della24si@mahasiswa.pcr.ac.id>
2025-11-19 11:03:11 +0700

    Penambahan Warga Sedeer

---
+++ database/seeders/WargaSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Warga;
+use Faker\Factory as Faker;
+
+class WargaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('id_ID');
+
+        for ($i = 1; $i <= 50; $i++) {
+            Warga::create([
+                'no_ktp'        => $faker->nik(),
+                'nama'          => $faker->name(),
+                'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+                'agama'         => $faker->randomElement(['Islam', 'Kristen', 'Katholik', 'Hindu', 'Buddha', 'Konghucu']),
+                'pekerjaan'     => $faker->jobTitle(),
+                'telp'          => $faker->phoneNumber(),
+                'email'         => $faker->unique()->safeEmail()
+            ]);
+        }
+    }
+}
