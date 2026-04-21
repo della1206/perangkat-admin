2e12367317589d37ab078d48d89c2e87b14b4db6
della <della24si@mahasiswa.pcr.ac.id>
2026-04-21 20:07:49 +0700

    ini

--- resources/views/layouts/Admin/app.blade.php
+++ resources/views/layouts/Admin/app.blade.php
@@ -1,12 +1,11 @@
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aplikasi Bina Desa</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
